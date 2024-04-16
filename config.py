4e70fb20240db156cc44fd46ba742497e79ff271
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-04-16 08:16:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6518790189:AAH18Rn2TSpRV2cDrFVF-Ruqy9CrChZ-F1s"
+    API_ID = "20717621"
+    API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
+    AUTH_USERS = '5934830127,1085174050'
+
