15fb7ffcc183554d1517bcfb81f19625b5508b4f
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-05 15:06:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6518790189:AAH18Rn2TSpRV2cDrFVF-Ruqy9CrChZ-F1s"
+    BOT_TOKEN = "6499994285:AAFaSySS9Fc50k9-CKvOPlZ5-Fcv7ZH7b28"
     API_ID = "20717621"
     API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
-    AUTH_USERS = '5934830127,1085174050'
+    AUTH_USERS = '1871785273'
 
