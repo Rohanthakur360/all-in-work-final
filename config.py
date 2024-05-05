ed68d268daa595780c3c4e0758210929e350007c
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-05 15:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = "6499994285:AAFaSySS9Fc50k9-CKvOPlZ5-Fcv7ZH7b28"
     API_ID = "20717621"
-    API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
-    AUTH_USERS = '1871785273'
+    API_HASH = ""
+    AUTH_USERS = ''
 
